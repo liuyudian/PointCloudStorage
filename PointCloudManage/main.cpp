5e69f177bed3285b8eb5198d62f12bf18f2db0a7
CrazyCake <291845444@qq.com>
2019-08-02 16:52:18 +0800

    新建界面类

--- PointCloudManage/main.cpp
+++ PointCloudManage/main.cpp
@@ -1,9 +1,9 @@
 #include "PointCloudManage.h"
-#include <iostream>
 #include <QtWidgets/QApplication>
 using namespace std;
 int main(int argc, char *argv[])
 {
+	cout << "hello world!" << endl;
 	QApplication a(argc, argv);
 	PointCloudManage w;
 	w.show();
