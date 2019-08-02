af4a1bf303d51ef5ad1ac4204bd547b2d72b6d7d
CrazyCake <291845444@qq.com>
2019-08-02 16:22:39 +0800

    test

--- PointCloudManage/main.cpp
+++ PointCloudManage/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-	cout << "hello world!" << endl;
+	cout << "hello world!123`23" << endl;
 	QApplication a(argc, argv);
 	PointCloudManage w;
 	w.show();
